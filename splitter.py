97f1e997e4d794a039212406568df55ba33f42a3
herenti <106109941+herenti@users.noreply.github.com>
2025-01-19 13:50:38 -0800

    Update splitter.py

--- splitter.py
+++ splitter.py
@@ -1,7 +1,6 @@
 import os
 import subprocess
-from pathlib import Path
-import stat
+
 import shlex
 
 todir = os.listdir(os.getcwd())
@@ -13,8 +12,6 @@ running = 1
 prepath = os.getcwd()+'/'
 
 processing = 0
-
-_script = open('split.sh', 'r').read()
 
 _dict = {}
 
@@ -30,11 +27,6 @@ while running == 1:
             _file = todir[i]
             _dest = prepath+i+'/'+_file
             os.mkdir(prepath+i)
-            f = open(prepath+i+"/split.sh", "w")
-            f.write(_script)
-            f.close()
-            f = Path(prepath+i+"/split.sh")
-            f.chmod(f.stat().st_mode | stat.S_IEXEC)
             os.rename(_file, _dest)
             os.rename(_dest, prepath+i+'/output.mp3')
             proc = subprocess.run(shlex.split('ffmpeg -i "'+prepath+i+'/output.mp3" -f segment -segment_time 14400 -c copy "'+prepath+i+'/Part%03d.mp3"'), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
