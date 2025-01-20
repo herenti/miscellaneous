a0d0f353a5c4bd10ab41d701bb8e0ff8f8e29950
herenti <106109941+herenti@users.noreply.github.com>
2025-01-20 11:40:33 -0800

    Add files via upload

---
+++ merger.py
@@ -0,0 +1,55 @@
+import os
+import subprocess
+import time
+import shlex
+
+todir = os.listdir(os.getcwd())
+mergefile = open('merge.sh', 'r').read()
+#extention = 'mp3'
+
+
+prepath = os.getcwd()+'/'
+
+to_process = {}
+
+def invoke_at(path: str):
+    def parameterized(func):
+        def wrapper(*args, **kwargs):
+            cwd = os.getcwd()
+            os.chdir(path)
+
+            try:
+                ret = func(*args, **kwargs)
+            finally:
+                os.chdir(cwd)
+
+            return ret
+
+        return wrapper
+
+    return parameterized
+
+for i in todir:
+    try:
+        a = os.listdir(prepath+i)
+        to_process[i] = a
+    except:
+        continue
+
+for i in to_process:
+    _file = prepath+i
+
+    @invoke_at(r'%s' % _file)
+    def start_the_tool():
+        print(os.getcwd())
+        f = open(_file+'/merge.sh', 'w')
+        f.write(mergefile)
+        f.close()
+        proc = subprocess.run(shlex.split('sh merge.sh'), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        out = proc.stdout
+        print(out)
+        for x in to_process[i]:
+            os.remove(x)
+
+    start_the_tool()
+
