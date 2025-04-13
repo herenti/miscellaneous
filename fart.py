7697a30ea44503d4879d1aa26dd11415737db47b
herenti <106109941+herenti@users.noreply.github.com>
2025-04-12 17:57:35 -0700

    Update fart.py

--- fart.py
+++ fart.py
@@ -4,7 +4,7 @@ def fart():
     choice = ['thbt','tttbbbbtt', 'tttttthhhhhhbbbbbttt', 'flubflublbblb','brrrrrraapppp','brrrurnnnntttt', ' ']
     _num = random.randrange(1, 5)
     choice = [random.choice(choice) for x in range(_num)]
-    if len(choice) == 1:
-        if choice[0] == ' ':
-            fart()
-    return ''.join(choice)
+    choice = ''.join(choice)
+    if choice == ' ':
+        choice = '   - It was a silent one....'
+    return choice
