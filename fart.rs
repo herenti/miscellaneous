d56f4526bcf9b15c6493ce81e9bb7c228ecbbfe8
herenti <106109941+herenti@users.noreply.github.com>
2025-05-21 14:01:36 -0700

    Update fart.rs

--- fart.rs
+++ fart.rs
@@ -1,16 +1,17 @@
 use rand::Rng;
+use std::io::{self, Write};
 
 fn fart() -> String{
     let choices = ["thbt","ttbbbtt", "tttttthhhbbbbbttt", "flubflublbblb","brrrrrraapppp","brrrurnnnntttt", " ", "shhhhppplaaatt", "surplat", "bromp"];
     let schoices = ["I feel a fart coming on... ", "I am so gassy... ", "Oh nooo im gonna fart... ", "I hope this is not a shart... ", "Please excuse my gas... ", "This might be a stinky one.... ", "Welcome to the fartfest... "];
-    let mut rng = rand::thread_rng();
-    let rangenum = rng.gen_range(1..4);
+    let mut rng = rand::rng();
+    let rangenum = rng.random_range(1..4);
     let mut _final = "".to_string();
     for i in 0..rangenum{
-        let num = rng.gen_range(0..choices.len());
+        let num = rng.random_range(0..choices.len());
         _final.push_str(choices[num]);
     };
-    let num = rng.gen_range(0..schoices.len());
+    let num = rng.random_range(0..schoices.len());
     let _final = if _final.chars().all(|c| c == ' ') {
         "It was a silent one"
     } else{
@@ -21,6 +22,10 @@ fn fart() -> String{
 
 fn main(){
 
-    println!("{}", fart());
+    println!("{}\r\n", fart());
+    print!("Press Enter to waft it away...");
+    io::stdout().flush().unwrap();
+    let mut _input = String::new();
+    io::stdin().read_line(&mut _input).unwrap();
 
 }
