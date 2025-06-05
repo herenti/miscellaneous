d469deace1ed0b60b8c9225d7e159fdf9f311b8a
herenti <106109941+herenti@users.noreply.github.com>
2025-06-04 17:02:22 -0700

    Create danbooru.rs

---
+++ danbooru.rs
@@ -0,0 +1,31 @@
+use reqwest::header::USER_AGENT;
+use reqwest::blocking::Client;
+
+pub struct Danbooru;
+
+impl Danbooru {
+
+    pub fn danbooru(tags: &str) -> String {
+
+            let url = format!("https://danbooru.donmai.us/posts.json?tags={}+order:random+rating:general&login=herenti&api_key=&limit=1", tags);
+            let client = reqwest::blocking::Client::new();
+            let res = client.get(url)
+            .header(USER_AGENT, "MyRustBot/0.1").send().expect("REASON").text().unwrap();
+            let data: serde_json::Value = serde_json::from_str(&res).unwrap();
+            if data[0]["media_asset"]["variants"].as_array().is_some(){
+                let result = &data[0]["media_asset"]["variants"];
+                let result = result.as_array().unwrap();
+                for i in result{
+                    if i["type"] == "original"{
+                        return i["url"].as_str().unwrap().to_string();
+                    }
+                }
+
+                "No result found.".to_string()
+
+            }
+            else{
+                "No results found. You can only search one tag.".to_string()
+            }
+    }
+}
